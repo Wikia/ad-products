804a87d20c01cd2113d0e0b9b7fdaef4e4829b77
Maciej Leśniewski <mlesniewski@wikia-inc.com>
2017-11-27 13:18:38 +0100

    ADEN-6132 Fixes teleporting of video UI bottom panel

--- src/modules/templates/uap/ui/panel.js
+++ src/modules/templates/uap/ui/panel.js
@@ -1,11 +1,13 @@
 export default class Panel {
 	constructor(className, uiElements) {
 		this.uiElements = uiElements;
-		this.panelContainer = document.createElement('div');
-		this.panelContainer.className = className;
+		this.className = className;
+		this.panelContainer = null;
 	}
 
 	add(video, container, params) {
+		this.panelContainer = document.createElement('div');
+		this.panelContainer.className = this.className;
 		this.uiElements.forEach((uiElement) => {
 			uiElement.add(video, this.panelContainer, params);
 		});
