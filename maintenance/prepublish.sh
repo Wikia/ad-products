afaf6884bc19703cba5f78e6983e2ce83511ce11
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-12-08 11:38:00 +0100

    ADEN-6363 Check branch before version/publish

--- maintenance/prepublish.sh
+++ maintenance/prepublish.sh
@@ -2,10 +2,10 @@
 
 CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
 
-#if [ $CURRENT_BRANCH != "dev" ]; then
-#	echo -e "\033[31mYou're not on dev branch\033[0m ($CURRENT_BRANCH)"
-#	exit 1
-#fi
+if [ $CURRENT_BRANCH != "dev" ]; then
+	echo -e "\033[31mYou're not on dev branch\033[0m ($CURRENT_BRANCH)"
+	exit 1
+fi
 
 CURRENT_VERSION=$(npm info ad-products version)
 
