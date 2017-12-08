0b4e7f4d6a2eb2bc789b9daf71f8a445695a283f
Maciej Leśniewski <mlesniewski@wikia-inc.com>
2017-12-08 17:58:45 +0100

    ADEN-6360 Fixes wrong body margin

--- src/modules/templates/uap/themes/hivi/ready.js
+++ src/modules/templates/uap/themes/hivi/ready.js
@@ -1,5 +1,6 @@
 import SlotTweaker from 'ad-engine/src/services/slot-tweaker';
 
-export function adIsReady({ adSlot }) {
-	return SlotTweaker.onReady(adSlot);
+export function adIsReady({ adSlot, params }) {
+	return SlotTweaker.onReady(adSlot)
+		.then(() => SlotTweaker.makeResponsive(adSlot, params.aspectRatio));
 }
