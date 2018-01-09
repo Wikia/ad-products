845fa6c03cc1a9c16620840ce306820aaf6dde35
nAndy <nandy@wikia-inc.com>
2018-01-09 11:47:43 +0100

    ADEN-6409 use requestAnimationFrame only for non-safari

--- src/modules/templates/uap/themes/hivi/hivi.js
+++ src/modules/templates/uap/themes/hivi/hivi.js
@@ -51,8 +51,12 @@ export class BfaaTheme extends BigFancyAdTheme {
 		if (ResolvedState.isResolvedState(this.params)) {
 			this.setResolvedState(true);
 		} else {
+			const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
 			ResolvedStateSwitch.updateInformationAboutSeenDefaultStateAd();
-			this.scrollListener = ScrollListener.addCallback(() => this.updateAdSizes());
+			this.scrollListener = ScrollListener.addCallback(
+				() => this.updateAdSizes(), !isSafari
+			);
 			// Manually run update on scroll once
 			this.updateAdSizes();
 		}
