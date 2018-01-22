626e3f63e55f4dbd9a542754d62fafaf0a129fd5
Damian Jozwiak <kvas-damian@users.noreply.github.com>
2018-01-22 11:58:48 +0100

    ADEN-6517 | HiVi CTP demo page

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,7 @@ const environments = {
 			'abcd': './examples/templates/abcd/script.js',
 			'floating-rail': './examples/templates/floating-rail/script.js',
 			'hivi-uap': './examples/templates/hivi-uap/script.js',
+			'hivi-uap-ctp': './examples/templates/hivi-uap-ctp/script.js',
 			'sticky-bfaa': './examples/templates/sticky-bfaa/script.js',
 			'vuap': './examples/templates/vuap/script.js'
 		},
