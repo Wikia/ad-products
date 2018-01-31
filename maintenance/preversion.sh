03da0914f651cb973dd4d6a8aee29649d8c72007
Maciej Leśniewski <mlesniewski@wikia-inc.com>
2018-01-31 10:20:22 +0100

    ADEN-6493 Maintenance scripts fails on any command

--- maintenance/preversion.sh
+++ maintenance/preversion.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
 
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
--- maintenance/version.sh
+++ maintenance/version.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
 
 npm run build --production
 git add dist/
