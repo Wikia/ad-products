cbc44034ddd4d5726dc114899c42cb0ee8588c6a
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-03-08 11:32:25 +0100

    ADEN-6614 Prepare test page

---
+++ examples/templates/hivi-uap-stickiness-not-allowed/script.js
@@ -0,0 +1,23 @@
+import { AdEngine, context, templateService } from '@wikia/ad-engine';
+import { BigFancyAdAbove, BigFancyAdBelow, FloatingRail } from '@wikia/ad-products';
+
+import customContext from '../../context';
+import '../../styles.scss';
+
+customContext.targeting.artid = '455';
+
+context.extend(customContext);
+
+if (document.body.offsetWidth < 728) {
+	context.set('state.isMobile', true);
+	context.set('targeting.skin', 'fandom_mobile');
+}
+
+templateService.register(BigFancyAdAbove);
+templateService.register(BigFancyAdBelow);
+templateService.register(FloatingRail);
+
+context.set('templates.bfaa.stickinessAllowed', false);
+context.set('templates.bfaa.mainContainer', document.querySelector('.header'));
+
+new AdEngine().init();
