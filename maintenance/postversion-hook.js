d59086a93c7fe472f1dc07a54278478c46cd98d8
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-03-29 11:21:23 +0200

    ADEN-6943 Add failure message

--- maintenance/postversion-hook.js
+++ maintenance/postversion-hook.js
@@ -3,28 +3,32 @@ const { IncomingWebhook } = require('@slack/client');
 const version = require('../package.json').version;
 const previousVersion = require('./previous-package.json').version;
 
-const webhookUrl = process.env.SLACK_INCOMING_WEBHOOK;
+const failed = process.argv[2] === 'failure';
 const repositoryName = 'ad-products';
+const buildUrl = process.env.BUILD_URL;
+const webhookUrl = process.env.SLACK_INCOMING_WEBHOOK;
 
 if (!webhookUrl) {
 	console.warn('Missing slack incoming webhook url. Message will not be sent.');
 	return;
 }
 
-if (!version) {
-	console.warn('Missing version tag. Message will not be sent.');
-	return;
-}
-
-const releaseLink = `<https://github.com/Wikia/${repositoryName}/releases/tag/v${version}|Release v${version}>`;
-let diffLink = '';
-
-if (previousVersion) {
-	diffLink = `(<https://github.com/Wikia/${repositoryName}/compare/v${previousVersion}...v${version}|show diff>)`;
-}
-
 const webhook = new IncomingWebhook(webhookUrl);
-const message = `New version of *${repositoryName}* has been released: ${releaseLink} ${diffLink}`;
+
+let message;
+
+if (failed) {
+	message = `:x: <!here|here>: Something went wrong during *${repositoryName}* release (<${buildUrl}|show log>)`;
+} else {
+	const releaseLink = `<https://github.com/Wikia/${repositoryName}/releases/tag/v${version}|Release v${version}>`;
+	let diffLink = '';
+
+	if (previousVersion) {
+		diffLink = `(<https://github.com/Wikia/${repositoryName}/compare/v${previousVersion}...v${version}|show diff>)`;
+	}
+
+	message = `New version of *${repositoryName}* has been released: ${releaseLink} ${diffLink}`;
+}
 
 webhook.send(message, (error, response) => {
 	if (error) {
