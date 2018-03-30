275e1a1ba6dc706c3c4888b723e6a890460b68c7
Jenkins <jenkins@fandom.com>
2018-03-30 10:49:06 +0000

    5.0.1

--- dist/ad-products.amd.js
+++ dist/ad-products.amd.js
@@ -6115,7 +6115,7 @@ if (get_default()(window, versionField, null)) {
 	window.console.warn('Multiple @wikia/ad-products initializations. This may cause issues.');
 }
 
-set_default()(window, versionField, 'v5.0.0');
+set_default()(window, versionField, 'v5.0.1');
 
 
 
