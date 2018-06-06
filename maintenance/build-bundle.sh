2115bd3e34620dd4efd806c60c1b39a4e9156bda
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-06-06 14:33:45 +0200

    Get rid of duplication in bundle

--- maintenance/build-bundle.sh
+++ maintenance/build-bundle.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-cat node_modules/@wikia/ad-engine/dist/ad-engine.global.js <(echo) ./dist/ad-products.global.js <(echo) ./dist/geo.global.js > ./dist/global-bundle.js
+cat node_modules/@wikia/ad-engine/dist/ad-engine.global.js <(echo) ./dist/ad-products.global.js > ./dist/global-bundle.js
