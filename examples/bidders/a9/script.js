68aeb07c292b0179df7fce5a8f35d80e2fb73f9b
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-08-07 11:35:39 +0200

    ADEN-7515 Add fake __cmp on example page

--- examples/bidders/a9/script.js
+++ examples/bidders/a9/script.js
@@ -5,6 +5,32 @@ import { bidders } from '@wikia/bidders';
 import customContext from '../../context';
 import '../../styles.scss';
 
+const optIn = true;
+
+window.__cmp = function (cmd, param, cb) {
+	if (cmd === 'getConsentData') {
+		cb({
+			consentData: optIn ? 'BOQu5jyOQu5jyCNABAPLBR-AAAAeCAFgAUABYAIAAaABFACY' : 'BOQu5naOQu5naCNABAPLBRAAAAAeCAAA',
+			gdprApplies: true,
+			hasGlobalScope: false
+		}, true);
+	} else if (cmd === 'getVendorConsents') {
+		cb({
+			metadata: 'BOQu5naOQu5naCNABAAABRAAAAAAAA',
+			purposeConsents: Array.from({length: 5}).reduce((map, val, i) => {
+				map[i + 1] = optIn;
+				return map;
+			}, {}),
+			vendorConsents: Array.from({length: 500}).reduce((map, val, i) => {
+				map[i + 1] = optIn;
+				return map;
+			}, {})
+		}, true);
+	} else {
+		cb(null, false);
+	}
+};
+
 context.extend(customContext);
 context.set('targeting.artid', '266');
 context.set('slots.incontent_boxad.disabled', false);
