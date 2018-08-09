28c6ba990d94189589cd1b9c3673b92d2b876fff
Jacek Bronski-Jankowski <jjankowski@wikia-inc.com>
2018-08-09 10:40:52 +0200

    ADEN-7457 Add bid key-vals to Porvata3.

--- src/bidders/prebid/prebid-settings.js
+++ src/bidders/prebid/prebid-settings.js
@@ -1,22 +1,39 @@
+import { context } from '@wikia/ad-engine';
+
 import { transformPriceFromCpm } from './price-helper';
 
 export function getSettings() {
 	return {
 		standard: {
 			alwaysUseBid: false,
-			adserverTargeting: [{
-				key: 'hb_bidder',
-				val: ({ bidderCode }) => bidderCode
-			}, {
-				key: 'hb_adid',
-				val: ({ adId }) => adId
-			}, {
-				key: 'hb_pb',
-				val: bidResponse => transformPriceFromCpm(bidResponse.cpm)
-			}, {
-				key: 'hb_size',
-				val: ({ size }) => size
-			}]
+			adserverTargeting: [
+				{
+					key: 'hb_bidder',
+					val: ({ bidderCode }) => bidderCode
+				},
+				{
+					key: 'hb_adid',
+					val: ({ adId }) => adId
+				},
+				{
+					key: 'hb_pb',
+					val: bidResponse => transformPriceFromCpm(bidResponse.cpm)
+				},
+				{
+					key: 'hb_size',
+					val: ({ size }) => size
+				},
+				{
+					key: 'hb_uuid',
+					val: bidResponse => (
+						(
+							bidResponse.bidderCode === 'appnexusAst' && context.get('custom.appnexusDfp')
+						) || (
+							bidResponse.bidderCode === 'rubicon' && context.get('custom.rubiconDfp')
+						) ? bidResponse.videoCacheKey : 'disabled'
+					)
+				}
+			]
 		}
 	};
 }
