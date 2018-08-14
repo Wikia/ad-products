52239556829e4bfda6e5b85f72a71c20da1f4f00
Jenkins <jenkins@fandom.com>
2018-08-14 11:57:49 +0000

    8.1.1

--- dist/ad-products.js
+++ dist/ad-products.js
@@ -5508,7 +5508,7 @@ if (get_default()(window, versionField, null)) {
 	window.console.warn('Multiple @wikia/ad-products initializations. This may cause issues.');
 }
 
-set_default()(window, versionField, 'v8.1.0');
+set_default()(window, versionField, 'v8.1.1');
 
 
 
