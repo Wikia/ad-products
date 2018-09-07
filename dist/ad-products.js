ea336e0215a4ad7fcc99d4da3dc9f5ee4b099296
Jenkins <jenkins@fandom.com>
2018-09-07 22:49:11 +0000

    9.2.1

--- dist/ad-products.js
+++ dist/ad-products.js
@@ -7877,7 +7877,7 @@ if (get_default()(window, versionField, null)) {
 	window.console.warn('Multiple @wikia/ad-products initializations. This may cause issues.');
 }
 
-set_default()(window, versionField, 'v9.2.0');
+set_default()(window, versionField, 'v9.2.1');
 
 
 
