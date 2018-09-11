3b592e07fd50c7ddd2197119433686c66f3eb4f3
Patryk Nather <pnather@wikia-inc.com>
2018-09-11 10:30:28 +0200

    Twitch template copied from BFAA, new example files

--- examples/templates/hivi-uap-twitch/script.js
+++ examples/templates/hivi-uap-twitch/script.js
@@ -1,22 +1,23 @@
 import { AdEngine, templateService, TwitchPlayer } from '@wikia/ad-engine';
-import { BigFancyAdAbove } from '@wikia/ad-products';
+import { BigFancyAdTwitch } from '@wikia/ad-products';
 
 import customContext from '../../context';
 import '../../styles.scss';
 
-customContext.targeting.artid = '390';
+customContext.targeting.artid = '415';
 
-templateService.register(BigFancyAdAbove);
+templateService.register(BigFancyAdTwitch);
 
-new AdEngine(customContext).init();
 
 // ===== UAP Twitch experiments =====
 const identifier = 'UAPTwitchTest';
 const options = {
-	width: 854,
-	height: 480,
-	channel: 'dallas',
+	width: 266,
+	height: 150,
+	channel: 'valkia',
 };
 const player = new TwitchPlayer(identifier, options);
-
 player.setVolume(0.75);
+
+
+new AdEngine(customContext).init();
--- src/templates/uap/index.js
+++ src/templates/uap/index.js
@@ -2,5 +2,6 @@ export * from './big-fancy-ad-above';
 export * from './big-fancy-ad-below';
 export * from './big-fancy-ad-in-player';
 export * from './universal-ad-package';
+export * from './big-fancy-ad-twitch';
 
 export { resolvedState } from './resolved-state';
