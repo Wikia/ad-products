396f1054e1ee52ed43d1ffbb6a1a4d6a3d1a2201
Patryk Nather <pnather@wikia-inc.com>
2018-09-12 14:20:05 +0200

    removed TwitchPlayer [added to BigFancyAdTwitch]

--- examples/templates/hivi-uap-twitch/script.js
+++ examples/templates/hivi-uap-twitch/script.js
@@ -1,4 +1,4 @@
-import { AdEngine, templateService, TwitchPlayer } from '@wikia/ad-engine';
+import { AdEngine, templateService } from '@wikia/ad-engine';
 import { BigFancyAdTwitch } from '@wikia/ad-products';
 
 import customContext from '../../context';
@@ -7,17 +7,5 @@ import '../../styles.scss';
 customContext.targeting.artid = '415';
 
 templateService.register(BigFancyAdTwitch);
-
-
-// ===== UAP Twitch experiments =====
-const identifier = 'UAPTwitchTest';
-const options = {
-	width: 266,
-	height: 150,
-	channel: 'valkia',
-};
-const player = new TwitchPlayer(identifier, options);
-player.setVolume(0.75);
-
 
 new AdEngine(customContext).init();
